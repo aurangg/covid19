99bd0f394f710cf53874616b2061e566dffbeeae
aurangg <aurangzebbbk@gmail.com>
2020-04-09 19:36:07 +0500

    Updates

---
+++ precache-manifest.987befff5340b36584cb081c45d69a38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea627e2ddca800fac360405af5190d7",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "a1afe2fae07cbd88f0c2",
+    "url": "/covid19/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "f4e2ff9b6dfd82fac4a9",
+    "url": "/covid19/static/css/main.6138b7e6.chunk.css"
+  },
+  {
+    "revision": "a1afe2fae07cbd88f0c2",
+    "url": "/covid19/static/js/2.9baf213f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid19/static/js/2.9baf213f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4e2ff9b6dfd82fac4a9",
+    "url": "/covid19/static/js/main.e712d203.chunk.js"
+  },
+  {
+    "revision": "412f75861195a6bfbb26",
+    "url": "/covid19/static/js/runtime-main.c8c89d2b.js"
+  }
+]);
